0b77a46025d2b742ebdae71c8d78c7ce6c35017f
Pascal Garber <pascal@artandcode.studio>
2022-10-19 13:17:40 +0200

    Removed vala experiment and cleanup

--- ref/Canvas.d.ts
+++
@@ -1,6 +0,0 @@
-interface Canvas {
-    height: number;
-    width: number;
-    getWebGLContext(options?: WebGLContextAttributes): WebGLRenderingContext | null;
-    getWebGL2Context(options?: WebGLContextAttributes): WebGLRenderingContext | null;
-}
